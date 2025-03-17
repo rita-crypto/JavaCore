e123484a1db8b27f9a90719d2667859cca43cb6a
rita-crypto <rita.khadka552@gmail.com>
2025-03-16 23:13:38 -0400

    Encapsulation practise question2

--- src/encapsulation/question2/Main.java
+++ src/encapsulation/question2/Main.java
@@ -1,5 +1,6 @@
 package encapsulation.question2;
-
+//Write a Java program to create a class called Employee with private instance variables employee_id, employee_name, and employee_salary.
+// Provide public getter and setter methods to access and modify the id and name variables, but provide a getter method for the salary variable that returns a formatted string.
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee();
